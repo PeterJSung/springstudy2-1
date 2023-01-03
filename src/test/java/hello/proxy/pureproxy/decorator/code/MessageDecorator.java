45979a9836b0cf81b9faebb59f68a5b8f3d97d13
jungmin.sung <jungmin.sung@linecorp.com>
2023-01-03 21:36:21 +0900

    refactor

--- src/test/java/hello/proxy/pureproxy/decorator/code/MessageDecorator.java
+++ src/test/java/hello/proxy/pureproxy/decorator/code/MessageDecorator.java
@@ -3,18 +3,16 @@ package hello.proxy.pureproxy.decorator.code;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class MessageDecorator implements Component {
-
-    private final Component component;
+public class MessageDecorator extends Decorator {
 
     public MessageDecorator(Component component) {
-        this.component = component;
+        super(component);
     }
 
     @Override
     public String operation() {
         log.info("Message Decorator 실행");
-        String result = component.operation();
+        String result = super.operation();
         String decoResult = "***" + result + "***";
         log.info("Message Decorator 꾸미기 before {} after {}", result, decoResult);
         return decoResult;
--- src/test/java/hello/proxy/pureproxy/decorator/code/TimeDecorator.java
+++ src/test/java/hello/proxy/pureproxy/decorator/code/TimeDecorator.java
@@ -3,19 +3,17 @@ package hello.proxy.pureproxy.decorator.code;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class TimeDecorator implements Component {
-
-    private final Component component;
+public class TimeDecorator extends Decorator {
 
     public TimeDecorator(Component component) {
-        this.component = component;
+        super(component);
     }
 
     @Override
     public String operation() {
         log.info("Time Decorator 실행");
         long startTime = System.currentTimeMillis();
-        String result = component.operation();
+        String result = super.operation();
         long endTime = System.currentTimeMillis();
         long resultTime = endTime-startTime;
         log.info("Time Decorator 종료 resultTIme={}", resultTime);
